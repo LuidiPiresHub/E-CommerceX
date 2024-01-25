e2ad5ac4b965897ffdd9d7688b3d3676027f1c7c
LuidiPires <luidihot@gmail.com>
2024-01-25 19:53:32 -0300

    refacotr: tela notfound redireciona para a ultima url

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,12 +1,13 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './NotFound.module.css';
 
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
     <main className={styles.notFoundContainer}>
       <h1 className={styles.notFoundTitle}>404</h1>
       <p className={styles.notFoundMessage}>Página não encontrada</p>
-      <Link className={styles.notFoundButton} to='/'>Voltar</Link>
+      <button type='button' onClick={() => navigate(-1)} className={styles.notFoundButton}>Voltar</button>
     </main>
   );
 }
