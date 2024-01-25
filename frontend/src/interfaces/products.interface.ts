3dd41f75dee4c297b8a108a3f6080baf7d2984c0
LuidiPires <luidihot@gmail.com>
2024-01-25 06:38:38 -0300

    feat: tela de detalhes de produto com responsivo em 90%

--- frontend/src/interfaces/products.interface.ts
+++ frontend/src/interfaces/products.interface.ts
@@ -1,9 +1,15 @@
+interface IPictures {
+  id: string;
+  url: string;
+}
+
 interface IProduct {
   id: string;
   title: string;
   price: number;
   thumbnail: string;
   quantity?: number;
+  pictures: IPictures[];
 }
 
 export default IProduct;
--- frontend/src/pages/productDetails/ProductDetails.tsx
+++ frontend/src/pages/productDetails/ProductDetails.tsx
@@ -4,38 +4,88 @@ import handleAxiosError from '../../axios/handleAxiosError';
 import axios, { AxiosError } from 'axios';
 import IProduct from '../../interfaces/products.interface';
 import EcommerceContext from '../../context/EcommerceContext';
+import styles from './ProductDetails.module.css';
+import { formartPrice } from '../../utils/functions';
+import Header from '../../components/header/Header';
+import { FaStar, FaRegHeart } from "react-icons/fa";
 
 export default function ProductDetails() {
+  const { error, setError, isLoading, setIsLoading } = useContext(EcommerceContext);
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<IProduct | null>(null);
-  const { error, setError, isLoading, setIsLoading } = useContext(EcommerceContext);
+  const [selectedImage, setSelectedImage] = useState<string>('');
 
-  
   useEffect(() => {
     const getProductById = async (id: string): Promise<void> => {
       try {
-        const { data } = await axios.get(`${import.meta.env.VITE_ML_ITEM_URL}/${id}`);
+        const { data } = await axios.get<IProduct>(`${import.meta.env.VITE_ML_ITEM_URL}/${id}`);
         setProduct(data);
+        setSelectedImage(data.pictures[0]?.url || '');
         setError(null);
-        setIsLoading(false);
       } catch (error) {
         setProduct(null);
         handleAxiosError(error as AxiosError, setError);
+      } finally {
+        setIsLoading(false);
       }
     };
-
     getProductById(id as string);
-  }, [id]);
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
 
   return (
-    <main>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <p>{JSON.stringify(product)}</p>
+    <>
+      <Header />
+      {product && (
+        <main className={styles.main}>
+          <div className={styles.containerWrapper}>
+            <section className={styles.productContainer}>
+              <aside className={styles.thumbnails}>{product.pictures.map(({ id, url }) => (
+                <img
+                  key={id}
+                  src={url}
+                  alt="Imagem alternativa do produto"
+                  className={`${styles.altImages} ${selectedImage === url && styles.selectedImage}`}
+                  onMouseOver={() => setSelectedImage(url)}
+                  onClick={() => setSelectedImage(url)}
+                />
+              ))}</aside>
+              <img src={selectedImage} alt="Imagem principal do produto" className={styles.mainImage} />
+            </section>
+            <aside className={styles.productDetails}>
+              <section className={styles.cardTop}>
+                <p>Novo | +100 vendidos</p>
+                <FaRegHeart className={styles.heart} />
+              </section>
+              <h1>{product.title}</h1>
+              <section className={styles.ratingContainer}>
+                <span>5.0</span>
+                <div>
+                  <FaStar className={styles.star} />
+                  <FaStar className={styles.star} />
+                  <FaStar className={styles.star} />
+                  <FaStar className={styles.star} />
+                  <FaStar className={styles.star} />
+                </div>
+                <span>(99+)</span>
+              </section>
+              <section className={styles.priceWrapper}>
+                <h2>{formartPrice(product.price)}</h2>
+                <h3>{`Em até 12x ${formartPrice(product.price / 12)}`}</h3>
+              </section>
+              <h4>O que você precisa saber sobre este produto:</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, similique quisquam aperiam pariatur ratione necessitatibus eaque dolorem vero deleniti officiis, blanditiis eos! Rem dignissimos placeat autem aliquid numquam harum tempora.</p>
+              <footer className={styles.buttonsContainer}>
+                <button type='button' className={`${styles.btn} ${styles.buyBtn}`}>Comprar agora</button>
+                <button type='button' className={`${styles.btn} ${styles.addCartBtn}`}>Adicionar ao carrinho</button>
+              </footer>
+            </aside>
+          </div>
+        </main>
       )}
-    </main>
+    </>
   );
 }
