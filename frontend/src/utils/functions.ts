da9836b2c87e41837eb29cb103413bf37e505f05
LuidiPires <luidihot@gmail.com>
2024-02-02 19:59:54 -0300

    fix: importação de interface

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -1,6 +1,6 @@
-import IProduct from '../interfaces/products.interface';
+import { IProductCart } from '../interfaces/products.interface';
 
 export const formartPrice = (price: number): string => price.toLocaleString('pt-br', { currency: 'BRL', style: 'currency' });
-export const countCartItems = (cartItems: IProduct[]): number => cartItems.reduce((acc, curr) => acc + curr.quantity!, 0);
-export const calcCartItemsPrice = (cartItems: IProduct[]): number => cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity!, 0);
-export const getHightestQuality = (image: string): string => image.replace(/\w\.jpg/gi, 'W.jpg') ;
+export const countCartItems = (cartItems: IProductCart[]): number => cartItems.reduce((acc, curr) => acc + curr.quantity!, 0);
+export const calcCartItemsPrice = (cartItems: IProductCart[]): number => cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity!, 0);
+export const getHightestQuality = (image: string): string => image.replace(/\w\.jpg/gi, 'W.jpg');
