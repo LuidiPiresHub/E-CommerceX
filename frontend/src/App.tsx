7c252b776f526a64d812cb52eda4421ff7a19c71
LuidiPires <luidihot@gmail.com>
2024-02-02 19:48:14 -0300

    feat: adiciona rotas de sucesso e falha de compra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Products from './pages/products/Products';
 import ProductDetail from './pages/productDetails/ProductDetails';
 import NotFound from './pages/notFound/NotFound';
 import Cart from './pages/cart/Cart';
+import CheckoutCancel from './pages/checkoutCancel/CheckoutCancel';
+import CheckoutSuccess from './pages/checkoutSuccess/CheckoutSuccess';
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path='/' element={<Products />} />
         <Route path='/product/:id' element={<ProductDetail />} />
         <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout/success' element={<CheckoutSuccess />} />
+        <Route path='/checkout/cancel' element={<CheckoutCancel />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </EcommerceProvider>
