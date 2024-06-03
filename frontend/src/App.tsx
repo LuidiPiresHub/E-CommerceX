195ad47b4c3571d903b1d8e9cd6ddc31c8997040
LuidiPires <luidihot@gmail.com>
2024-06-03 11:03:17 -0300

    feat: mensagem de boas vindas e rotas de perfil e favorito

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,40 @@
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 import { Routes, Route } from 'react-router-dom';
 import EcommerceProvider from './context/EcommerceProvider';
 import Products from './pages/products/Products';
 import ProductDetail from './pages/productDetails/ProductDetails';
-import NotFound from './pages/notFound/NotFound';
 import Cart from './pages/cart/Cart';
-import CheckoutCancel from './pages/checkoutCancel/CheckoutCancel';
-import CheckoutSuccess from './pages/checkoutSuccess/CheckoutSuccess';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import Favorites from './pages/favorites/Favorites';
+import CheckoutCancel from './pages/checkoutCancel/CheckoutCancel';
+import CheckoutSuccess from './pages/checkoutSuccess/CheckoutSuccess';
+import NotFound from './pages/notFound/NotFound';
+import Profile from './pages/profile/Profile';
 
 export default function App() {
+  useEffect(() => {
+    const hideWelcomeMessage = JSON.parse(localStorage.getItem('hideWelcomeMessage')!);
+    if (hideWelcomeMessage) return;
+    Swal.fire({
+      icon: 'info',
+      title: 'Bem-vindo à E-CommerceX!',
+      html: 'Gostaria de informar que a E-CommerceX é uma demonstração fictícia. <br><br>Nenhum dos produtos exibidos são reais, e os detalhes de pagamento também são simulados.',
+      input: 'checkbox',
+      inputAutoFocus: false,
+      inputPlaceholder: 'Não mostrar novamente',
+      inputValidator: (value) => {
+        const boolValue = Boolean(value);
+        if (boolValue) {
+          localStorage.setItem('hideWelcomeMessage', JSON.stringify(boolValue));
+        }
+      },
+      confirmButtonText: 'Entendi',
+      confirmButtonColor: 'rgb(48, 133, 214)',
+    });
+  }, []);
+
   return (
     <EcommerceProvider>
       <Routes>
@@ -18,6 +43,8 @@ export default function App() {
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/checkout/success' element={<CheckoutSuccess />} />
         <Route path='/checkout/cancel' element={<CheckoutCancel />} />
         <Route path='*' element={<NotFound />} />
