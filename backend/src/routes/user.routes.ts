59fd4f42d8cd7cfb31bd099de260ce6095b808ec
LuidiPires <luidihot@gmail.com>
2024-06-11 14:43:59 -0300

    fix: rota de upload novamente

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import usersController from '../controller/user.controller';
 import validateToken from '../middlewares/validadeToken';
-// import { upload } from '../multer/multerConfig';
+import { upload } from '../multer/multerConfig';
 
 const usersRouter = Router();
 
@@ -9,6 +9,6 @@ usersRouter.get('/', validateToken, usersController.getUser);
 usersRouter.post('/register', usersController.register);
 usersRouter.post('/login', usersController.login);
 usersRouter.post('/logout', validateToken, usersController.logout);
-usersRouter.put('/:id', validateToken, usersController.updateUser);
+usersRouter.put('/:id', validateToken, upload.single('profileImg'), usersController.updateUser);
 
 export default usersRouter;
