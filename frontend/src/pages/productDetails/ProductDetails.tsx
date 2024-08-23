a045e910fb2d7fdc8e3404b26edd8a43d078222d
LuidiPires <luidihot@gmail.com>
2024-08-23 02:02:43 -0300

    feat: nova cor do spinner e mensagem de erro

--- frontend/src/pages/productDetails/ProductDetails.tsx
+++ frontend/src/pages/productDetails/ProductDetails.tsx
@@ -95,7 +95,7 @@ export default function ProductDetails() {
       <ThreeCircles
         height="100"
         width="100"
-        color="#4fa94d"
+        color="#0076CE"
         ariaLabel="three-circles-loading"
       />
     </div>
