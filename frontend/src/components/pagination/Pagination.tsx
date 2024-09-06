1ea0083bf550ef8ce91e40595e821bca3ff268df
LuidiPires <luidihot@gmail.com>
2024-09-06 17:59:07 -0300

    feat: componente de paginação

--- frontend/src/components/pagination/Pagination.tsx
+++ frontend/src/components/pagination/Pagination.tsx
@@ -5,22 +5,22 @@ import { IPaginationProps } from '../../interfaces/pagination.interface';
 export default function Pagination({ pageCount, forcePage, onPageChange }: IPaginationProps) {
   return (
     <ReactPaginate
-    breakLabel=""
-    nextLabel="Next"
-    onPageChange={onPageChange}
-    pageRangeDisplayed={2}
-    marginPagesDisplayed={1}
-    pageCount={pageCount}
-    previousLabel="Previous"
-    renderOnZeroPageCount={null}
-    containerClassName={styles.pagination}
-    pageLinkClassName={styles.pageLink}
-    previousLinkClassName={styles.previous}
-    nextLinkClassName={styles.next}
-    activeLinkClassName={styles.active}
-    breakLinkClassName={styles.break}
-    disabledLinkClassName={styles.disabledLink}
-    forcePage={forcePage}
-  />
-  )
+      breakLabel=""
+      nextLabel="Next"
+      onPageChange={onPageChange}
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={1}
+      pageCount={pageCount}
+      previousLabel="Previous"
+      renderOnZeroPageCount={null}
+      containerClassName={styles.pagination}
+      pageLinkClassName={styles.pageLink}
+      previousLinkClassName={styles.previous}
+      nextLinkClassName={styles.next}
+      activeLinkClassName={styles.active}
+      breakLinkClassName={styles.break}
+      disabledLinkClassName={styles.disabledLink}
+      forcePage={forcePage}
+    />
+  );
 }
