d5b5dd0f91a1b5159b7c25879cba5e2fc7ca6d88
LuidiPires <luidihot@gmail.com>
2024-09-10 11:26:56 -0300

    refactor: deleta contexto global

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,10 @@ import { ToastContainer } from 'react-toastify';
 import App from './App.tsx';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './index.css';
-import { GlobalProvider } from './context/GlobalContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <GlobalProvider>
-      <ToastContainer />
-      <App />
-    </GlobalProvider>
+    <ToastContainer />
+    <App />
   </BrowserRouter>
 );
