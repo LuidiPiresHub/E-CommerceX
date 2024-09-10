ec460a8b6da395db6e214ac3aa51febd55bb3955
LuidiPires <luidihot@gmail.com>
2024-09-10 11:22:51 -0300

    feat: crud e validações da rota de carrinho

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import productRoutes from './product.routes';
 import stripeRoutes from './stripe.routes';
 import userRoutes from './user.routes';
+import cartRouter from './cart.routes';
 
 export default {
   productRoutes,
   stripeRoutes,
   userRoutes,
+  cartRouter,
 };
